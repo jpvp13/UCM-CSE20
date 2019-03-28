cd75473a95455faf21559794222dcf078c0c125f
jpvp13 <33247490+jpvp13@users.noreply.github.com>
2019-03-27 17:39:09 -0700

    Add files via upload

---
+++ Lab 7/NumberFINAL.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Number {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Please enter a number 0-25: ");
+		double num = input.nextInt();
+		
+		System.out.println("You have entered " + num);
+	}
+
+}
