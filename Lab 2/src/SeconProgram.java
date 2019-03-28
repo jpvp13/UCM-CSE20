4792069c82960ed466c2abd3c8e0acbd5ed6f981
jpvp13 <33247490+jpvp13@users.noreply.github.com>
2019-03-27 17:32:30 -0700

    Add files via upload

---
+++ Lab 2/src/SeconProgram.java
@@ -0,0 +1,18 @@
+
+public class SeconProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi, I interviewed Andy.");
+		System.out.println("Andy recieved a 3 on his AP Computer Science class at his highschool.");
+		System.out.println("Andy doesn't think this course will be as hard as some think.");
+		System.out.print("He possibly also wants to be able to code vide games. ");
+		System.out.print("He said he is also super interested in making app creator.");
+		//Collaborated with Andy Lopez
+		
+	}
+
+}
